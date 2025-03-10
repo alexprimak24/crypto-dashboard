63ee97487ea051ad02ba1397373b458978a9def6
alexprimak24 <aprimak43@gmail.com>
2025-03-10 21:47:39 +0200

    feat: implemented search for coin ability

--- src/pages/HomeDashboard.tsx
+++ src/pages/HomeDashboard.tsx
@@ -1,15 +1,39 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import MarketSummary from '../components/MarketSummary';
 import SearchForCoin from '../components/SearchForCoin';
 import CoinsList from '../components/CoinsList';
+import { useTopCryptoList } from '../hooks/useTopCryptoList';
+
+const COINS_TO_SHOW = 10;
 
 function HomeDashboard() {
   const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const topCryptoData = useTopCryptoList(
+    import.meta.env.VITE_COINGECO_KEY,
+    page,
+    COINS_TO_SHOW,
+  );
+
+  const filteredTopCryptoList = useMemo(() => {
+    if (!topCryptoData.topCryptoList) return [];
+    return topCryptoData.topCryptoList.filter((topCrypto) =>
+      topCrypto.name.toLowerCase().includes(query.toLowerCase()),
+    );
+  }, [topCryptoData.topCryptoList, query]);
+
   return (
     <div className="mx-auto flex w-full max-w-7xl flex-col gap-5">
       <MarketSummary />
       <SearchForCoin query={query} setQuery={setQuery} />
-      <CoinsList />
+      <CoinsList
+        topCryptoData={{
+          topCryptoList: filteredTopCryptoList,
+          isLoading: topCryptoData.isLoading,
+          error: topCryptoData.error,
+        }}
+        setPage={setPage}
+      />
     </div>
   );
 }

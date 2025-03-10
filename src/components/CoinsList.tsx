a90d6e2c147f7c0db8e26b1e267e0ee7102fbce5
alexprimak24 <aprimak43@gmail.com>
2025-03-10 21:48:15 +0200

    Added explanation for the columns that are being displayed

--- src/components/CoinsList.tsx
+++ src/components/CoinsList.tsx
@@ -1,22 +1,34 @@
-import React, { useState } from 'react';
-import { useTopCryptoList } from '../hooks/useTopCryptoList';
+import React from 'react';
 import CoinItem from './CoinItem';
 import Spinner from './ui/Spinner';
 import Message from './ui/Message';
+import { CryptoList } from '../services/api/types';
 
-const COINS_TO_SHOW = 10;
+interface CoinListProps {
+  topCryptoData: {
+    topCryptoList: CryptoList[] | null;
+    isLoading: boolean;
+    error: string;
+  };
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+}
 // CHECK OUT ABOUT AMOUNT OF RERENDERS once I finish with that component
-export default function CoinsList() {
-  const [page, setPage] = useState(1);
-  const { topCryptoList, isLoading, error } = useTopCryptoList(
-    import.meta.env.VITE_COINGECO_KEY,
-    page,
-    COINS_TO_SHOW,
-  );
+export default function CoinsList({ topCryptoData, setPage }: CoinListProps) {
+  const { topCryptoList, isLoading, error } = topCryptoData;
 
   return (
     <div className="min-h-screen px-4 pt-2 pb-8">
       <div className="mx-auto max-w-7xl">
+        <div className="mt-2 grid w-full grid-cols-9 items-center gap-4 rounded-lg bg-[#242424] p-4 text-right shadow transition-shadow hover:shadow-md">
+          <h4 className="col-span-1 text-center font-semibold text-gray-300">
+            Rank
+          </h4>
+          <h4 className="col-span-2 flex items-center gap-2">Name</h4>
+          <h4 className="col-span-1 font-semibold text-gray-100">Price</h4>
+          <h4 className="col-span-1 text-sm text-gray-400">ATH</h4>
+          <h4 className="col-span-2 text-right">24h %</h4>
+          <h4 className="col-span-2 text-center">Supply</h4>
+        </div>
         {isLoading && <Spinner />}
         {error && (
           <Message message="Something went wrong while fetching topCryptoList" />
